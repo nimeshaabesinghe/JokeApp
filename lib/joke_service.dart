c1bd785e77b133da335d228d1d8f9e2ff7013af5
Nimesha Abesinghe <abesinghenimesha20@gmail.com>
2024-12-14 10:09:36 +0530

    added gradient effect

--- lib/joke_service.dart
+++ lib/joke_service.dart
@@ -1,26 +1,19 @@
-// import 'package:dio/dio.dart';
-//
-// class JokeService {
-//   final Dio _dio = Dio();
-//
-//   Future<List<Map<String, dynamic>>> fetchJokesRaw() async {
-//     try {
-//       final response = await _dio.get(
-//         '$_baseUrl/Programming',
-//         queryParameteres: {
-//           'amount': 3,
-//           'type': 'single,twopart',
-//           'blacklistFlags' :'nsfw,religious,political',
-//         },
-//       );
-//       if (response.statusCode == 200) {
-//         final List<dynamic> jokesJson = response.data['jokes'];
-//         return jokesJson.cast<Map<String, dynamic >>();
-//       } else {
-//         throw Exception('Failed to load jokes');
-//       }
-//     } catch (e) {
-//       throw Exception('Error fetching jokes: $e');
-//     }
-//   }
-// }
+import 'package:dio/dio.dart';
+
+class JokeService {
+  final Dio _dio = Dio();
+
+  Future<List<Map<String, dynamic>>> fetchJokesRaw() async {
+    try {
+      final response = await _dio.get('https://v2.jokeapi.dev/joke/Any?amount=3&blacklistFlags=nsfw');
+      if (response.statusCode == 200) {
+        final List<dynamic> jokesJsonList = response.data['jokes'];
+        return jokesJsonList.cast<Map<String, dynamic>>();
+      } else {
+        throw Exception("Failed to load jokes");
+      }
+    } catch (e) {
+      throw Exception("Error fetching jokes: $e");
+    }
+  }
+}
